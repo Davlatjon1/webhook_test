f02a3731ea364dde0cdc35ec30b14d801f8ab8c5
Davlatjon99 <abdudavlat99@gmail.com>
2020-10-17 22:29:34 +0500

    added a file

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+TOKEN = '1393668330:AAGpfdN4_W5Se5mCz3RHfsYnVF2CDViDfcY'
+database_name = 'music.db'
+shelve_name = 'shelve.db'
+
+botan_key = 'a2f57e95-7220-4a26-bb08-9db3540e3de4'
+
